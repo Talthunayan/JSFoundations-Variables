7b6f56494aec111eb37ae3d9c6ae825b9f60d2e2
Talthunayan <78157264+Talthunayan@users.noreply.github.com>
2021-02-01 18:51:20 +0300

    Task 3 for the story

---
+++ story.js
@@ -0,0 +1,8 @@
+// Task 3
+
+let name0="Taiba";
+let age=22;
+let hobby="Coding"
+
+Age_plus_33=age+33
+console.log("Mr./Mrs."+" "+name0+","+" in 33 years, you will be "+Age_plus_33+" years old. By then you'll have dacades of "+hobby+" practice");
